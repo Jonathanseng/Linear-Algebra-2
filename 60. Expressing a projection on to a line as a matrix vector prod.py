40314919b3c75e5aa7cf66a8b8daee3796196ea1
Seng Keat <corodostudio@gmail.com>
2023-08-02 12:48:31 +0700

    expressiong a projection on to a line as a matric vector

---
+++ 60. Expressing a projection on to a line as a matrix vector prod.py
@@ -0,0 +1,64 @@
+import math
+
+
+def projection(matrix, v):
+  """
+  Returns the projection of v onto the direction of matrix.
+
+  Args:
+    matrix: The matrix representing the direction of projection.
+    v: The vector to project.
+
+  Returns:
+    The projection of v onto the direction of matrix.
+  """
+
+  projection_matrix = matrix * matrix.T
+  projection_vector = projection_matrix * v
+  return projection_vector
+
+
+def dot(matrix, v):
+  """
+  Returns the dot product of matrix and v.
+
+  Args:
+    matrix: The matrix.
+    v: The vector.
+
+  Returns:
+    The dot product of matrix and v.
+  """
+
+  result = 0
+  for i in range(len(matrix)):
+    row = 0
+    for j in range(len(matrix[0])):
+      row += matrix[i][j] * v[j]
+    result += row
+
+  return result
+
+
+def unit_vector(v):
+  """
+  Returns the unit vector in the direction of v.
+
+  Args:
+    v: The vector to find the unit vector of.
+
+  Returns:
+    The unit vector in the direction of v.
+  """
+
+  magnitude = math.sqrt(v[0]**2 + v[1]**2 + v[2]**2)
+  if magnitude == 0:
+    return [0, 0, 0]
+  else:
+    return [v[0] / magnitude, v[1] / magnitude, v[2] / magnitude]
+
+
+if __name__ == "__main__":
+  matrix = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+  v = [1, 2, 3]
+  print(projection(matrix, v))

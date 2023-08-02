c9821738f0697181a511386586bffc31d51359f8
Seng Keat <corodostudio@gmail.com>
2023-08-02 08:32:04 +0700

    matrices reduced row echelon form 3

---
+++ 32. Matrices: Reduced row echelon form 3.py
@@ -0,0 +1,37 @@
+def rref(matrix):
+  """Computes the reduced row echelon form of a matrix."""
+  n = len(matrix)
+  for i in range(n):
+    # Find the pivot element in column i.
+    pivot_row = i
+    for j in range(i + 1, n):
+      if abs(matrix[j][i]) > abs(matrix[pivot_row][i]):
+        pivot_row = j
+
+    # Swap rows i and pivot_row.
+    if pivot_row != i:
+      matrix[i], matrix[pivot_row] = matrix[pivot_row], matrix[i]
+
+    # Make all other entries in column i equal to 0.
+    for j in range(n):
+      if j != i:
+        factor = matrix[j][i] / matrix[i][i]
+        for k in range(n):
+          matrix[j][k] -= factor * matrix[i][k]
+
+    # Make all leading entries equal to 1, and make all other entries below the leading
+    # entry equal to 0.
+    if matrix[i][i] != 0:
+      for j in range(n):
+        if j > i:
+          factor = matrix[j][i] / matrix[i][i]
+          for k in range(n):
+            matrix[j][k] -= factor * matrix[i][k]
+          matrix[j][i] = 0
+
+  return matrix
+
+if __name__ == "__main__":
+  matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+  rref_matrix = rref(matrix)
+  print("The reduced row echelon form of the matrix is:", rref_matrix)

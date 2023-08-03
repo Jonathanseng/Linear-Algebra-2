bbbe78d7dec6075cfcef561fdf2ccafec59e6acb
Seng Keat <corodostudio@gmail.com>
2023-08-03 08:23:31 +0700

    duplicate row determinant

---
+++ 81. Determinants along other rows/86. Duplicate row determinant.py
@@ -0,0 +1,38 @@
+def determinant(matrix):
+  """
+  Calculates the determinant of a matrix.
+
+  Args:
+    matrix: A 2D list of numbers.
+
+  Returns:
+    The determinant of the matrix.
+  """
+
+  if len(matrix) == 1:
+    return matrix[0][0]
+  else:
+    sum = 0
+    for i in range(len(matrix)):
+      sub_matrix = matrix[:]
+      sub_matrix.pop(i)
+      sum += matrix[i][0] * determinant(sub_matrix) * (-1 if i % 2 else 1)
+    return sum
+
+def determinant_duplicate_row(matrix):
+  """
+  Calculates the determinant of a matrix when a row is duplicated.
+
+  Args:
+    matrix: A 2D list of numbers.
+
+  Returns:
+    The determinant of the matrix.
+  """
+
+  return 0
+
+if __name__ == "__main__":
+  matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+  print(determinant(matrix))
+  print(determinant_duplicate_row(matrix))

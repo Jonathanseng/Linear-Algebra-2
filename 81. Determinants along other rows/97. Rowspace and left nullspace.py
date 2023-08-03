a410b7e0b57123c21bb9e71cff9c5d59c5a9c5c6
Seng Keat <corodostudio@gmail.com>
2023-08-03 08:35:09 +0700

    rowspace and left nullspace

---
+++ 81. Determinants along other rows/97. Rowspace and left nullspace.py
@@ -0,0 +1,41 @@
+def rowspace(matrix):
+  """
+  Calculates the rowspace of a matrix.
+
+  Args:
+    matrix: The matrix.
+
+  Returns:
+    The rowspace of the matrix.
+  """
+
+  rowspace = []
+  for i in range(len(matrix)):
+    row = matrix[i]
+    rowspace.append(row)
+  return rowspace
+
+def left_nullspace(matrix):
+  """
+  Calculates the left nullspace of a matrix.
+
+  Args:
+    matrix: The matrix.
+
+  Returns:
+    The left nullspace of the matrix.
+  """
+
+  left_nullspace = []
+  for i in range(len(matrix)):
+    row = [0 for _ in range(len(matrix[0]))]
+    row[i] = 1
+    left_nullspace.append(row)
+  return left_nullspace
+
+if __name__ == "__main__":
+  matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+  rowspace = rowspace(matrix)
+  print(rowspace)
+  left_nullspace = left_nullspace(matrix)
+  print(left_nullspace)

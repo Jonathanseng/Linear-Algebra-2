c794ce7577d164a57acb79be82f3aa62d5b0293a
Seng Keat <corodostudio@gmail.com>
2023-08-03 08:37:20 +0700

    orthogonal complement

---
+++ 81. Determinants along other rows/99. Orthogonal complements.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def orthogonal_complement(matrix):
+  """
+  Calculates the orthogonal complement of a matrix.
+
+  Args:
+    matrix: The matrix.
+
+  Returns:
+    The orthogonal complement of the matrix.
+  """
+
+  rowspace = rowspace(matrix)
+  left_nullspace = left_nullspace(matrix)
+  orthogonal_complement = []
+  for i in range(len(matrix[0])):
+    row = [0 for _ in range(len(matrix[0]))]
+    row[i] = 1
+    if row not in rowspace and row not in left_nullspace:
+      orthogonal_complement.append(row)
+  return orthogonal_complement
+
+if __name__ == "__main__":
+  matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+  orthogonal_complement = orthogonal_complement(matrix)
+  print(orthogonal_complement)

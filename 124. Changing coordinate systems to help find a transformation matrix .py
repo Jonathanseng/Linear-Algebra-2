00f958e4f64d613580159325e75999ad2a3df272
Seng Keat <corodostudio@gmail.com>
2023-08-04 08:44:31 +0700

    changing coordinate system

---
+++ 124. Changing coordinate systems to help find a transformation matrix .py
@@ -0,0 +1,40 @@
+import numpy as np
+
+def change_of_basis(basis_vectors):
+  """
+  Returns the invertible change of basis matrix for the given basis vectors.
+
+  Args:
+    basis_vectors: A list of NumPy arrays representing the basis vectors.
+
+  Returns:
+    The invertible change of basis matrix.
+  """
+
+  change_of_basis_matrix = np.transpose(basis_vectors)
+  if np.linalg.det(change_of_basis_matrix) == 0:
+    raise ValueError("The given basis vectors do not form an invertible basis.")
+
+  return np.linalg.inv(change_of_basis_matrix)
+
+def main():
+  # Define the basis vectors.
+  basis_vectors = np.array([[1, 0], [0, 1]])
+
+  # Calculate the change of basis matrix.
+  change_of_basis_matrix = change_of_basis(basis_vectors)
+
+  # Print the change of basis matrix.
+  print(change_of_basis_matrix)
+
+  # Define a vector in the first basis.
+  vector_in_first_basis = np.array([1, 0])
+
+  # Transform the vector to the second basis.
+  vector_in_second_basis = np.dot(change_of_basis_matrix, vector_in_first_basis)
+
+  # Print the vector in the second basis.
+  print(vector_in_second_basis)
+
+if __name__ == "__main__":
+  main()
